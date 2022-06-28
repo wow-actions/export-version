2ec2013767855e34058e265835103e828190bbe9
bubkoo <bubkoo.wy@gmail.com>
2022-06-28 10:22:46 +0800

    chore: 🛠️ remove verbose code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import path from 'path'
+// import path from 'path'
 import mustache from 'mustache'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
